e5d28b67603f977b38c4620ea1f04bb09bea559f
Engr. Hamid Idrees <hamididrees457@gmail.com>
2026-02-14 18:41:46 +0500

    Add project files.

---
+++ MvcCrudApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCrudApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
