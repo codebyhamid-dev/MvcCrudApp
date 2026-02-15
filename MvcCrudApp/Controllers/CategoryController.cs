b820b83b645c31cf43cc0f823a54014e3d4d8328
Engr. Hamid Idrees <hamididrees457@gmail.com>
2026-02-15 10:50:20 +0500

    Implement Catgory

---
+++ MvcCrudApp/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcCrudApp.Data;
+using MvcCrudApp.Models;
+
+namespace MvcCrudApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category/Index
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                // No dropdown here, but you can return the view with the invalid model
+                return View(category);
+            }
+            // Will only reach here if ModelState is valid
+            if (string.IsNullOrWhiteSpace(category.Description))
+                category.Description = null;
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
